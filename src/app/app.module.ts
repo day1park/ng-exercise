559d48b19ecd284620e2e0a9b20f30856ecc00a5
day1park <daypark2408@gmail.com>
2019-02-23 19:12:59 +1100

    ng g c components/ExerciseItem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExercisesComponent } from './components/exercises/exercises.component';
+import { ExerciseItemComponent } from './components/exercise-item/exercise-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExercisesComponent
+    ExercisesComponent,
+    ExerciseItemComponent
   ],
   imports: [
     BrowserModule,
