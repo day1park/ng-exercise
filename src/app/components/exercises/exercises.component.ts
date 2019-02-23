0975b417300ec0f3979031b65445e3326d81aeb5
day1park <daypark2408@gmail.com>
2019-02-23 19:08:24 +1100

    create models folder with Exercise and import to exercises component

--- src/app/components/exercises/exercises.component.ts
+++ src/app/components/exercises/exercises.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Exercise } from "../../models/Exercise";
 
 @Component({
-  selector: 'app-exercises',
-  templateUrl: './exercises.component.html',
-  styleUrls: ['./exercises.component.css']
+  selector: "app-exercises",
+  templateUrl: "./exercises.component.html",
+  styleUrls: ["./exercises.component.css"]
 })
 export class ExercisesComponent implements OnInit {
+  exercises: Exercise[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
