68269f8da182e42b352bd66e6085c12652767816
day1park <daypark2408@gmail.com>
2019-02-23 19:01:05 +1100

    ng generate component components/Exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExercisesComponent } from './components/exercises/exercises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExercisesComponent
   ],
   imports: [
     BrowserModule,
