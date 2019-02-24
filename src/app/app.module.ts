e2c1a00be3e7680478c8270be58526f20e61db6d
day1park <daypark2408@gmail.com>
2019-02-24 20:07:26 +1100

    fix component misspelling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ExercisesComponent } from "./components/exercises/exercises.component";
 import { ExerciseItemComponent } from "./components/exercise-item/exercise-item.component";
-import { HeaderComponent } from './components/layout/header/header.component';
-import { AddExerciseComponent } from './componenets/add-exercise/add-exercise.component';
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { AddExerciseComponent } from "./components/add-exercise/add-exercise.component";
 
 @NgModule({
-  declarations: [AppComponent, ExercisesComponent, ExerciseItemComponent, HeaderComponent, AddExerciseComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ExercisesComponent,
+    ExerciseItemComponent,
+    HeaderComponent,
+    AddExerciseComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/add-exercise/add-exercise.component.ts
+++ src/app/components/add-exercise/add-exercise.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-exercise',
-  templateUrl: './add-exercise.component.html',
-  styleUrls: ['./add-exercise.component.css']
+  selector: "app-add-exercise",
+  templateUrl: "./add-exercise.component.html",
+  styleUrls: ["./add-exercise.component.css"]
 })
 export class AddExerciseComponent implements OnInit {
+  title: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
