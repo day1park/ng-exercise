e00ca66148f744db9190cd1cd74c49058fdc9109
day1park <daypark2408@gmail.com>
2019-02-24 20:24:07 +1100

    add static about me page for routing prac

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ExercisesComponent } from "./components/exercises/exercises.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
-const routes: Routes = [{ path: "", component: ExercisesComponent }];
+const routes: Routes = [
+  { path: "", component: ExercisesComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
